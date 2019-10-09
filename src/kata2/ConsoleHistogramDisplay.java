27b58ad5398543504b646deed4508f4642b9a8c4
JeremyTrujillo <>
2019-10-09 13:29:04 +0100

    Kata 2 using a HistogramDisplay interface

---
+++ src/kata2/ConsoleHistogramDisplay.java
@@ -0,0 +1,15 @@
+package kata2;
+
+public class ConsoleHistogramDisplay implements HistogramDisplay{
+    
+    public ConsoleHistogramDisplay() {
+    }
+
+    @Override
+    public void show(Histogram histogram) {
+        for (Integer value : histogram) {
+            System.out.println(value + " " + histogram.getCounts(value));
+        }    
+    }
+
+}
